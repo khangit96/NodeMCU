23bbd6f43f993d4cd21433d2371caf4d2e854754
khangit96 <khangit69@gmail.com>
2017-03-19 12:33:48 +0700

    fkfkd

--- smartWaterBottle/smartWaterBottle.ino
+++ smartWaterBottle/smartWaterBottle.ino
@@ -6,9 +6,10 @@
 // Firebase Config.
 #define FIREBASE_HOST "demonodemcu.firebaseio.com"
 #define FIREBASE_AUTH "fNDh456Nv6xtdBPqJtwcXHEN7JfMCWYs44LozYeb"
-#define WIFI_SSID "OpenLAB-2"
-#define WIFI_PASSWORD "merry=x-mas"
+#define WIFI_SSID "Du Du Duc"
+#define WIFI_PASSWORD "0985589587"
 #define led D0
+#define sound
 
 //Hsrf04 measure distance of water
 const int triggerPin = D5; // Trigger Pin
@@ -18,6 +19,7 @@ int minimumRange = 2; // Minimum range needed adalah 2 cm
 float duration;
 float distance; // Duration used to calculate distance
 float distanceBefore=100;
+int count=0;
 
 /*Setup*/
 void setup()
@@ -36,6 +38,7 @@ void setup()
     Serial.println("Server started");
     Serial.println(WiFi.localIP());
     pinMode(led, OUTPUT);
+    pinMode(4, OUTPUT);
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
@@ -49,6 +52,54 @@ void putFloat(float value)
         Serial.println(Firebase.error());
         return;
     }
+}
+void configWifi(char  tenWifi[100],char matKhau[100])
+{    
+    int count=0;
+    WiFi.mode(WIFI_OFF);
+    WiFi.begin(tenWifi,matKhau);
+    Serial.print("Reconnecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+        
+        count++;
+        if(count==20)
+        {
+          ReConfigWifi();
+          Serial.print("Connect Failed");
+          Firebase.pushBool("wifiSuccess",false);
+          return; 
+        }
+    }
+      Serial.print("Connect Success");
+      digitalWrite(led,HIGH);
+      delay(1000);
+      digitalWrite(led,LOW);
+      delay(1000);
+      digitalWrite(led,HIGH);
+      delay(1000);
+      digitalWrite(led,LOW);
+      delay(1000);
+      digitalWrite(led,HIGH);
+      delay(1000);
+      digitalWrite(led,LOW);
+      delay(1000);
+      digitalWrite(led,HIGH);
+      delay(1000);
+      Firebase.pushBool("wifiSuccess",true);
+}
+void ReConfigWifi()
+{    
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("Server started");
+    Serial.println(WiFi.localIP());
+  
 }
 
 /*Function measure distance of water*/
@@ -68,33 +119,47 @@ void measureDistance(){
     Serial.println("HC-SR 04 sensor out of measurement range");
   }
  else {
-   // if(distance>distanceBefore){
+  
        Serial.print("Distance:");
        Serial.print(distance);
        Serial.println("cm");
- //   }
-    // Serial.print("Distance Before:");
-     //Serial.print(distanceBefore);
-     //Serial.println("cm");
-
-     //float fix=distance-distanceBefore;
-     //Serial.println(fix);
-    //distanceBefore=distance;
-   
-   putFloat(distance);
+       putFloat(distance);
   }
 
 }
 
 /*Function Loop*/
-void loop(){
- /*if (Firebase.getBool("state")){
-    digitalWrite(led,LOW);
-    measureDistance();
- }
- else{
-   digitalWrite(led,HIGH);
- }*/
- measureDistance();
+void loop()
+{
+  if(Firebase.getBool("status"))
+  {
+        digitalWrite(led,LOW);
+        if (Firebase.getBool("WifiChange"))
+        { 
+          Firebase.setBool("WifiChange",false);
+          
+          String str_MatKhau=Firebase.getString("Wifi/matKhau");
+          String str_TenWifi=Firebase.getString("Wifi/tenWifi");
+           
+          char tenWifi[100];
+          char matKhau[100];
+          str_MatKhau.toCharArray(matKhau, 100) ;
+          str_TenWifi.toCharArray(tenWifi, 100) ;
+          
+         configWifi(tenWifi,matKhau);
+         }
+
+    //Nếu người dùng hẹn giờ
+      if(Firebase.getBool("Alarm")
+      {
+        Firebase.setBool("Alarm",false);
+         digitalWrite(4,HIGH);
+      }
+  }
+  else
+  {
+       digitalWrite(led,HIGH);
+  }
+
  delay(2000);
 }
